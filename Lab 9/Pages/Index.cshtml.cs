cb01ef5fd091590b7fa78a709c1a26446a504353
WastedTM <kilivnika62@gmail.com>
2023-11-06 20:05:16 +0200

    Final commit lab 9

---
+++ Lab 9/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab_9.Pages;
+
+public class Product : PageModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public List<Product> Products { get; set; }
+
+    public void OnGet()
+    {
+        Products = new List<Product>
+        {
+            new Product { ID = 1, Name = "Pizza", Price = 10.99 },
+            new Product { ID = 2, Name = "Sushi", Price = 30.50 },
+            new Product { ID = 3, Name = "Ice cream", Price = 3 }
+        };
+    }
+}
---
+++ Lab 9/Pages/WeatherPage.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Lab_9.Pages
+{
+    public class WeatherPage : PageModel
+    {
+        private const string url = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=32e6ad85dc05cdc5cb3d1cc0aa578b66&units=metric";
+
+        public string? Message { get; set; }
+
+        public string? WeatherName { get; set; }
+        public double WeatherTemp { get; set; }
+        public double WeatherFeelsLike { get; set; }
+        public string? WeatherMain { get; set; }
+
+        public async Task OnGet()
+        {
+            WeatherData weatherData = await GetWeather();
+    
+            WeatherName = weatherData.Name;
+            WeatherTemp = weatherData.Main.Temp;
+            WeatherFeelsLike = weatherData.Main.Feels_Like;
+            WeatherMain = weatherData.Weather[0].Main; // Отримайте значення з першого елемента масиву
+
+            Message = $"Погода в місті {WeatherName}: {WeatherMain}, Температура - {WeatherTemp}\u00b0C, Відчувається як - {WeatherFeelsLike}\u00b0C";
+        }
+
+        public async Task<WeatherData> GetWeather()
+        {
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+            
+            WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(response)!;
+
+            return weatherData;
+        }
+    }
+    
+    public class WeatherData
+    {
+        public string Name { get; set; }
+        public MainData Main { get; set; }
+        public Weather[] Weather { get; set; }
+    }
+
+    public class MainData
+    {
+        public double Temp { get; set; }
+        public double Feels_Like { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Main { get; set; }
+    }
+}
